6fed63b143ef17666d8c677d47b4b327960c4d81
Lakshy-R <Lakshyrupani.lr@gmail.com>
2025-03-06 12:43:31 +0200

    Changes to user schema

--- user.js
+++ user.js
@@ -1,14 +1,33 @@
 const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
-    name: String,
-    email: Strting,
-    profilePicture: String,
-    password: String,
-    role: String,
+    name: {
+      String,
+      required: true,
+   },
+    email: {
+      Strting,
+      required: true,
+   },
+
+    profilePicture: {
+      String, 
+   },
+    password: {
+      String,
+      required: true,
+   },
+    role: {
+      String,
+      enum: ['User', 'Organizer', 'System Admin'],
+      required: true,
+    },
     createdAt:{
        type: Date,
        immutable: true,
        default: () => Date.now(),
     }
     })
+
+const User = mongoose.model('User', userSchema)
+module.exports = User
