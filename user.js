12a3477d3cf767169026ba66b6c94f4585a2b1f1
Lakshy-R <Lakshyrupani.lr@gmail.com>
2025-03-06 13:05:27 +0200

    spelling mistake

--- user.js
+++ user.js
@@ -1,33 +1,32 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     name: {
-      String,
-      required: true,
-   },
-    email: {
-      String,
-      required: true,
-   },
-
-    profilePicture: {
-      String, 
-   },
-    password: {
-      String,
-      required: true,
-   },
-    role: {
-      String,
-      enum: ['User', 'Organizer', 'System Admin'],
-      required: true,
+        type: String,
+        required: true,
     },
-    createdAt:{
-       type: Date,
-       immutable: true,
-       default: () => Date.now(),
-    }
-    })
+    email: {
+        type: String,  
+        required: true,
+    },
+    profilePicture: {
+        type: String, 
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    role: {
+        type: String,
+        enum: ['User', 'Organizer', 'System Admin'],
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        immutable: true,
+        default: () => Date.now(),
+    },
+});
 
-const User = mongoose.model('User', userSchema)
-module.exports = User
+const User = mongoose.model('User', userSchema);
+module.exports = User;
