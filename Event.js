57772a98b43a58b5cd6706645189e7b34a105691
Amr <kamr08985@gmail.com>
2025-03-07 23:06:29 +0200

    fixed timestamp

--- Event.js
+++ Event.js
@@ -1,49 +1,50 @@
-const mongoose=require("mongoose")
-const eventSchema=new mongoose.Schema({
- title:{
-    type:String,
-    required:true
- },
- description:{
-    type:String,
-    required:true
- },
- date:{
-    type:Date,
-    required:true
- },
- location:{
-    type:String,
-    required:true
- },
- category:{
-    type:String,
-    required:true
- },
- image:{
-    type:String,
- },
- ticketPrice:{
-    type:Number,
-    required:true,
-    min:0
- },
-remainingTickets:{
-    type:Number,
-    required:true,
-    min:0
-},
-totalNumberOfTickets:{
-    type:Number,
-    required:true,
-    min:0
-},
-Organizer:{
-    type:mongoose.Schema.Types.ObjectId,
-    ref:"User",
-    required:true
-},
-timestamps:true
-})
+const mongoose = require('mongoose');
+
+const eventSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+    },
+    ticketPrice: {
+        type: Number,
+        required: true,
+        min: 0
+    },
+    remainingTickets: {
+        type: Number,
+        required: true,
+        min: 0
+    },
+    totalNumberOfTickets: {
+        type: Number,
+        required: true,
+        min: 0
+    },
+    Organizer: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    }
+}, { timestamps: true });
+
 const Event = mongoose.model("Event", eventSchema);
-module.exports = Event;
+module.exports = Event;
