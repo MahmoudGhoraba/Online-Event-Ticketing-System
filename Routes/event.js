f0859e84881a14645712ba5f75fbc01c67569f09
Amr <kamr08985@gmail.com>
2025-04-14 21:29:40 +0200

    Added new path to the route of event

--- Routes/event.js
+++ Routes/event.js
@@ -5,7 +5,8 @@ const authenticationMiddleware=require('../Middleware/authenticationMiddleware')
 const router=express.Router();
 router.post("/",authenticationMiddleware,authorizationMiddleware["Organizer"],eventController.createEvent);
 router.get("/",eventController.getPostedEvents);
-///api/v1/events/:id GET Get details of a single event Public
+//api/v1/events/:id GET Get details of a single event Public
+router.get("/:id",eventController.getSingleEvent);
 router.put("/:id",authenticationMiddleware,authorizationMiddleware["Organizer","Admin"],eventController.updateEvent);
 router.delete("/:id",authenticationMiddleware,authorizationMiddleware["Organizer","Admin"],eventController.deleteEvent);
 module.exports=router;
