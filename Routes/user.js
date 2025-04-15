0deb27032a644bdad98de7aa1c81326278abed9e
MahmoudGhoraba <mahmoud.ghoraba@student.giu-uni.de>
2025-04-15 20:43:30 +0200

    implemented API routes for user/events

--- Routes/user.js
+++ Routes/user.js
@@ -6,6 +6,7 @@ const bookingController=require("../Controllers/BookingController")
 
 router.get("/",/*MIIDLEWARE ADMIN ONLY authorizationMiddleware(['Admin']),*/userController.getUsers)
 router.get("/profile",userController.getUserById)
+router.get("/events",userController.getUserEvents)
 router.put("/profile",userController.updateUser)
 
 router.get("/:id",/*MIIDLEWARE ADMIN ONLY authorizationMiddleware(['Admin']),*/userController.getUserById)
