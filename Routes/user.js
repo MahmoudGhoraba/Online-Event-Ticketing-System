9c42e6e5da5291130b76845518c6db2bb6951cef
Amr <kamr08985@gmail.com>
2025-04-18 22:16:33 +0200

    fix error

--- Routes/user.js
+++ Routes/user.js
@@ -3,15 +3,17 @@ const router=express.Router()
 const userController=require("../Controllers/userController")
 const eventController=require("../Controllers/eventController")
 const bookingController=require("../Controllers/BookingController")
+const authorizationMiddleware=require('../Middleware/authorizationMiddleware');
+const authenticationMiddleware=require('../Middleware/authenticationMiddleware')
 
 router.get("/",authenticateMiddleware,authorizeMiddleware(["Admin"]),userController.getUsers)//HENA ESMAHA USERS
 
 // here we put all types====>authenticated user
-router.get("/profile",authenticateMiddleware,authorizeMiddleware(['Admin','User','Organizer']),userController.getUserProfile)
-router.put("/profile",authenticateMiddleware,authorizeMiddleware(['Admin','User','Organizer']),userController.updateUserProfile)
-router.get("/bookings",authenticateMiddleware,authorizationMiddleware(['User']),bookingController.getUserBookings)
-router.get("/events",authenticateMiddleware,authorizationMiddleware(['Oragnizer']),userController.getUserEvents) // thats an error reminder
-router.get("/events/analytics",authenticateMiddleware,authorizationMiddleware(['Oragnizer']),eventController.getOrganizerEventAnalytics)
-router.get("/:id",authenticateMiddleware,authorizationMiddleware(['Admin']),userController.getUserById)
-router.put("/:id",authenticateMiddleware,authorizationMiddleware(['Admin']),userController.updateUserRole)
-router.delete("/:id",authenticateMiddleware,authorizationMiddleware(['Admin']),userController.deleteUser)
+router.get("/profile",authenticationMiddleware,authorizeMiddleware(['Admin','User','Organizer']),userController.getUserProfile)
+router.put("/profile",authenticationMiddleware,authorizeMiddleware(['Admin','User','Organizer']),userController.updateUserProfile)
+router.get("/bookings",authenticationMiddleware,authorizationMiddleware(['User']),bookingController.getUserBookings)
+router.get("/events",authenticationMiddleware,authorizationMiddleware(['Oragnizer']),userController.getUserEvents) // thats an error reminder
+router.get("/events/analytics",authenticationMiddleware,authorizationMiddleware(['Oragnizer']),eventController.getOrganizerEventAnalytics)
+router.get("/:id",authenticationMiddleware,authorizationMiddleware(['Admin']),userController.getUserById)
+router.put("/:id",authenticationMiddleware,authorizationMiddleware(['Admin']),userController.updateUserRole)
+router.delete("/:id",authenticationMiddleware,authorizationMiddleware(['Admin']),userController.deleteUser)
