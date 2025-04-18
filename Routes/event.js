a61ed57b191acee623358d504a538f7162e4013b
Amr <kamr08985@gmail.com>
2025-04-19 01:35:56 +0200

    fixed the bracket problem

--- Routes/event.js
+++ Routes/event.js
@@ -3,10 +3,10 @@ const express=require("express");
  const authorizationMiddleware=require('../Middleware/authorizeMiddleware');
  const authenticationMiddleware=require('../Middleware/authenticateMiddleware')
  const router=express.Router();
- router.post("/",authenticationMiddleware,authorizationMiddleware["Organizer"],eventController.createEvent);
+ router.post("/",authenticationMiddleware,authorizationMiddleware(["Organizer"]),eventController.createEvent);
  router.get("/",eventController.getPostedEvents);
  //api/v1/events/:id GET Get details of a single event Public
  router.get("/:id",eventController.getSingleEvent);
- router.put("/:id",authenticationMiddleware,authorizationMiddleware["Organizer","Admin"],eventController.updateEvent);
- router.delete("/:id",authenticationMiddleware,authorizationMiddleware["Organizer","Admin"],eventController.deleteEvent);
+ router.put("/:id",authenticationMiddleware,authorizationMiddleware(["Organizer","Admin"]),eventController.updateEvent);
+ router.delete("/:id",authenticationMiddleware,authorizationMiddleware(["Organizer","Admin"]),eventController.deleteEvent);
  module.exports=router;
