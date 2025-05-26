031f488fbb6bd4b9ca69732fa9a06b1d60e613b3
MahmoudGhoraba <mahmoud.ghoraba@student.giu-uni.de>
2025-05-26 23:33:18 +0300

    toast

---
+++ frontend/src/sharedComponents/Toast.jsx
@@ -0,0 +1,28 @@
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+export const Toast = () => {
+  return (
+    <ToastContainer
+      position="top-right"
+      autoClose={3000}
+      hideProgressBar={false}
+      newestOnTop
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="colored"
+    />
+  );
+};
+
+export const showToast = {
+  success: (message) => toast.success(message),
+  error: (message) => toast.error(message),
+  info: (message) => toast.info(message),
+  warning: (message) => toast.warning(message),
+};
+
+export default Toast; 
